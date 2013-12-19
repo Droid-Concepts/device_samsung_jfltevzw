97eecab81cbe25621ede42471cb4d70e164b7cc6
davros <davros@droidconcepts.com>
2013-12-19 17:02:30 -0500

    fingureprint

--- dc.mk
+++ dc.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/Droid_Concepts/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/Droid_Concepts/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltevzw TARGET_DEVICE=jfltevzw BUILD_FINGERPRINT="Verizon/jfltevzw/jfltevzw:4.2.2/JDQ39/I545VRUAMDK:user/release-keys" PRIVATE_BUILD_DESC="jfltevzw-user 4.2.2 JDQ39 I545VRUAMDK release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltevzw TARGET_DEVICE=jfltevzw BUILD_FINGERPRINT="Verizon/jfltevzw/jfltevzw:4.3/JSS15J/I545VRUEMK2:user/release-keys" PRIVATE_BUILD_DESC="jfltevzw-user 4.4.3 JSS15J I545VRUEMK2 release-keys"
 
 PRODUCT_NAME := dc_jfltevzw
 PRODUCT_DEVICE := jfltevzw
